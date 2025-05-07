28632d6d9cab00a7d3bea03f57ee7920292861f2
felipe <felipemg@amazon.com>
2025-05-07 18:12:38 +0000

    Added Docker build and push

--- app-cdk/app_cdk/pipeline_cdk_stack.py
+++ app-cdk/app_cdk/pipeline_cdk_stack.py
@@ -5,13 +5,17 @@ from aws_cdk import (
     CfnOutput,
     aws_codepipeline as codepipeline,
     aws_codebuild as codebuild,
-    aws_codepipeline_actions as codepipeline_actions
+    aws_codepipeline_actions as codepipeline_actions,
+    aws_iam as iam,
 
 )
+import os
+
+
 
 class PipelineCdkStack(Stack):
 
-    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str,ecr_repository, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # Creates a CodeConnections resource called 'CICD_Workshop_Connection'
@@ -27,6 +31,8 @@ class PipelineCdkStack(Stack):
             execution_mode=codepipeline.ExecutionMode.QUEUED
         )
 
+        
+
         code_quality_build = codebuild.PipelineProject(
             self, 'CodeBuild',
             build_spec = codebuild.BuildSpec.from_source_filename('./buildspec_test.yml'),
@@ -39,6 +45,8 @@ class PipelineCdkStack(Stack):
 
         source_output = codepipeline.Artifact()
         unit_test_output = codepipeline.Artifact()
+        docker_build_output = codepipeline.Artifact()
+
 
         source_action = codepipeline_actions.CodeStarConnectionsSourceAction(
             action_name = 'GitHub',
@@ -67,6 +75,60 @@ class PipelineCdkStack(Stack):
             actions = [build_action]
         )
 
+        docker_build_project = codebuild.PipelineProject(
+            self, 'Docker Build',
+            build_spec = codebuild.BuildSpec.from_source_filename('./buildspec_docker.yml'),
+            environment = codebuild.BuildEnvironment(
+                build_image = codebuild.LinuxBuildImage.STANDARD_5_0,
+                privileged = True,
+                compute_type = codebuild.ComputeType.LARGE,
+                environment_variables = {
+                    'IMAGE_TAG': codebuild.BuildEnvironmentVariable(
+                        type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+                        value = 'latest'
+                    ),
+                    'IMAGE_REPO_URI': codebuild.BuildEnvironmentVariable(
+                        type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+                        value = ecr_repository.repository_uri
+                    ),
+                    'AWS_DEFAULT_REGION': codebuild.BuildEnvironmentVariable(
+                        type = codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+                        value = os.environ['CDK_DEFAULT_REGION']
+                    )
+                }
+            ),
+        )
+
+        docker_build_project.add_to_role_policy(iam.PolicyStatement(
+            effect = iam.Effect.ALLOW,
+            actions = [
+                'ecr:GetAuthorizationToken',
+                'ecr:BatchCheckLayerAvailability',
+                'ecr:GetDownloadUrlForLayer',
+                'ecr:GetRepositoryPolicy',
+                'ecr:DescribeRepositories',
+                'ecr:ListImages',
+                'ecr:DescribeImages',
+                'ecr:BatchGetImage',
+                'ecr:InitiateLayerUpload',
+                'ecr:UploadLayerPart',
+                'ecr:CompleteLayerUpload',
+                'ecr:PutImage'
+            ],
+            resources = ['*'],
+        ))
+
+        docker_build_action = codepipeline_actions.CodeBuildAction(
+            action_name = 'Docker-Build',
+            project = docker_build_project,
+            input = source_output,
+            outputs = [docker_build_output]
+        )
+
+        pipeline.add_stage(
+            stage_name = 'Docker-Push-ECR',
+            actions = [docker_build_action]
+        )
         CfnOutput(
             self, 'SourceConnectionArn',
             value = SourceConnection.attr_connection_arn
